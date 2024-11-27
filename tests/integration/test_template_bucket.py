7b47fa007cf3bdf310492ba70f46c665815eef11
cmac <>
2024-11-27 15:01:35 -0500

    correctly check status code

--- tests/integration/test_template_bucket.py
+++ tests/integration/test_template_bucket.py
@@ -1,4 +1,5 @@
 def test_template_bucket_exists(s3_client):
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/head_bucket.html
     # TODO: don't hardcode name
     response = s3_client.head_bucket(Bucket="sam-webshell-templates")
-    assert response["status_code"] == 200
+    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
