34e68071594fe0db92e2463c83482fa0f6176569
cmac <>
2024-12-11 14:54:05 -0500

    refactored for clarity

--- documents/app.py
+++ documents/app.py
@@ -24,7 +24,7 @@ logger.setLevel("INFO")
 
 
 class S3Resource:
-    """AWS S3 Resource Class"""
+    """AWS S3 Resource"""
 
     def __init__(self, lambda_s3_resource):
         """Initialize an S3 Resource"""
@@ -33,20 +33,28 @@ class S3Resource:
         self.bucket = self.resource.Bucket(self.bucket_name)
 
 
-def download_template(templates_resource, key, download_path):
+def download_template(s3resource: S3Resource, *, key: str, filename: str) -> bool:
+    # key - name of key in source bucket
+    # filename - path downloaded file
     try:
-        templates_resource.bucket.download_file(key, download_path)
-        logger.info(f"template: {key} downloaded")
+        s3resource.bucket.download_file(key, filename)
+        logger.info(
+            f"template: {key} downloaded from {s3resource.bucket_name} as {filename}"
+        )
     except ClientError as e:
         logger.error(e)
         return False
     return True
 
 
-def upload_generated_document(templates_resource, upload_path, key):
+def upload_generated_document(
+    s3resource: S3Resource, *, key: str, filename: str
+) -> bool:
+    # key - name of key in target bucket
+    # filename - path of file to upload
     try:
-        templates_resource.bucket.upload_file(upload_path, key)
-        logger.info(f"{key} created in {templates_resource.bucket_name} bucket")
+        s3resource.bucket.upload_file(filename, key)
+        logger.info(f"{key} created in {s3resource.bucket_name} bucket as {filename}")
     except ClientError as e:
         logger.error(e)
         return False
@@ -62,11 +70,12 @@ def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
 
         print("###EVENT RECIEVED", event)
 
+        generated_document_url = "https://{bucket}.s3.amazonaws.com/{key}"
         download_path = f"/tmp/template-{uuid.uuid4()}.docx"
         template = json.loads(event["body"])["template"]
 
         template_downloaded = download_template(
-            s3resource_templates, template, download_path
+            s3resource_templates, key=template, filename=download_path
         )
         if not template_downloaded:
             raise Exception("Failed to get template")
@@ -74,14 +83,16 @@ def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
         # upload_path = f"/tmp/generated-{uuid.uuid4()}.docx"
         upload_path = download_path
 
-        # TODO: get generated docket name from dynamodb
+        # TODO: get generated document template name from dynamodb
         # TODO: generated documents shold be under /documents key
-        key = "test.docx"
+        generated_document_key = "test.docx"
 
         global _S3_RESOURCE_GENERATED_DOCUMENTS
         s3resource_generated_documents = S3Resource(_S3_RESOURCE_GENERATED_DOCUMENTS)
         uploaded_generated_document = upload_generated_document(
-            s3resource_generated_documents, key, upload_path
+            s3resource_generated_documents,
+            key=generated_document_key,
+            filename=upload_path,
         )
         if not uploaded_generated_document:
             raise Exception("Failed to upload generated document")
@@ -92,7 +103,9 @@ def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
             "statusCode": 201,
             "headers": {
                 "Content-Type": "application/json",
-                "Location": f"https://{s3resource_generated_documents.bucket_name}.s3.amazonaws.com/{key}",
+                "Location": generated_document_url.format(
+                    bucket=s3resource_generated_documents, key=generated_document_key
+                ),
             },
         }
 
