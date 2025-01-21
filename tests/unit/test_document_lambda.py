62a6a7e394f3be4630f94fbd34e7ecd846596a89
cmac <>
2025-01-21 16:21:30 -0500

    added comments

--- tests/unit/test_document_lambda.py
+++ tests/unit/test_document_lambda.py
@@ -18,6 +18,11 @@ def upload_to_s3_resource(
         resource.bucket.upload_file(docx_file, f"{prefix}/{filename}.docx")
 
 
+# TODO: first refactor to send context as body of request then do this
+# def get_text_from_generated_document(response):
+#     pass
+
+
 @pytest.mark.usefixtures("patched_s3_resource_generated_documents")
 @pytest.mark.parametrize("event", ["blank_template_doc"], indirect=True)
 def test_valid_POST_event_returns_200_and_location(
@@ -109,3 +114,20 @@ def test_valid_GET_request_lists_available_templates(
     assert (
         json.loads(response["body"])[0] == "documents/blank_template_doc.docx"
     ), "Did not find document in bucket"
+
+
+# TODO: first refactor to send context as body of request then do this
+# @pytest.mark.parametrize("event", ["general_amendment"], indirect=True)
+# def test_passed_in_content_appears_in_generated_document(
+#     patched_s3_resource_templates, event, pytestconfig
+# ):
+#     upload_to_s3_resource(
+#         patched_s3_resource_templates,
+#         pytestconfig.rootpath,
+#         ["blank_template_doc"],
+#         "documents",
+#     )
+#     response = lambda_handler(event=event, context=None)
+#     assert response["statusCode"] == 201
+#     text = get_text_from_generated_document(response)
+#     assert "ABC-123US01" in text
